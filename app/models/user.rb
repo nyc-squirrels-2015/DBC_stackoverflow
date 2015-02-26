152e60320ecd47e13fe96f8226ee9e3a2edfab34
Jonathan Cho <joncho26@gmail.com>
2015-02-26 17:32:01 -0500

    add User model with associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :questions
   has_many :answers
-  has_many :comments, as: :commentable
-  has_many :votes, as: :votable
+  has_many :comments
+  has_many :votes
 end
