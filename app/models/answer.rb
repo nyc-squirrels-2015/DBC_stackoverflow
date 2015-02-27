0a6a706c4b80216a7b5834a964c9e4b71ccdc507
Jonathan Cho <joncho26@gmail.com>
2015-02-26 20:10:44 -0500

    add validations for answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,8 @@ class Answer < ActiveRecord::Base
   belongs_to :question
   has_many :comments, as: :commentable
   has_many :votes, as: :votable
+
+  validates :content, presence: true
+  validates :user, presence: true
+  validates :question, presence: true
 end
