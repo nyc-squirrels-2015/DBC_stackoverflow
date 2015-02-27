8a69360d93aa5f8598042e2278f2b0953925c6f3
Jonathan Cho <joncho26@gmail.com>
2015-02-27 15:25:39 -0500

    add logic in show route, created private method for comment params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,21 @@
 class CommentsController < ApplicationController
   def show
+    @comment = Comment.find(params[:id])
   end
 
   def new
     comment = Comment.new
+  end
+
+  def create
+    comment = Comment.create(comment_params)
+  end
+
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit([:content,:user,:commentable])
   end
 end
