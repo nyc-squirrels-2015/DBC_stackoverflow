49b9e2478e1b6c6c425bc462ef53a2675146b2d3
Jonathan Cho <joncho26@gmail.com>
2015-02-26 19:07:01 -0500

    add validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   has_many :answers
   has_many :comments
   has_many :votes
+
+  validates :name, presence:true, uniqueness: true
+  validates :password, presence: true
 end
